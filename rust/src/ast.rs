08934082f86813cca83aed2646eb49275c034b4c
Patrick Flynn <pflynn157@gmail.com>
2023-10-23 12:58:34 -0400

    Unlicensed programs

--- rust/src/ast.rs
+++ rust/src/ast.rs
@@ -1,4 +1,10 @@
 //
+// This software is licensed under BSD0 (public domain).
+// Therefore, this software belongs to humanity.
+// See COPYING for more info.
+//
+
+//
 // Contains the AST for our python interpreter
 //
 
--- rust/src/lex.rs
+++ rust/src/lex.rs
@@ -1,3 +1,8 @@
+//
+// This software is licensed under BSD0 (public domain).
+// Therefore, this software belongs to humanity.
+// See COPYING for more info.
+//
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
